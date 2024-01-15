cfb0ccf3371d1d13500a8bb283b14bc5e6a7a554
dnis-pc <dbode0694@gmail.com>
2024-01-15 13:02:00 -0500

    adding animation to bio

--- src/Components/Bio/Bio.component.jsx
+++ src/Components/Bio/Bio.component.jsx
@@ -18,15 +18,15 @@ const Bio = () =>{
 
             <div className="container">  
 
-                <div id = 'imageContainer'>
+                <div id = 'imageContainer' className="animate glow delay-1">
                     <img src={photo} id='profilePhoto' alt="" />
                 </div>
 
                 <div>
                     
                     <div className='detailsContainer'>
-                        <h3 className='deatilHeaderh3'>So...Who am I?</h3>
-                        <p className='customWidth bioText'>Lorem ipsum dolor sit amet
+                        <h3 className='deatilHeaderh3 animate glow delay-1'>So...Who am I?</h3>
+                        <p className='customWidth animate glow delay-1'>Lorem ipsum dolor sit amet
                             consectetur adipisicing 
                             elit. Veritatis, distinctio. 
                             Molestiae soluta minus, 
@@ -38,14 +38,14 @@ const Bio = () =>{
                         </p>                
 
                         {/* change this into maping over an imported object */}
-                        <h4>Programming Languages & Frameworks</h4>
-                        <TechCollection techMastered={TECHS.LENGUAGES_MASTERED}/>
+                        <h4 className="animate glow delay-3">Programming Languages & Frameworks</h4>
+                        <TechCollection techMastered={TECHS.LENGUAGES_MASTERED} className="animate glow delay-3"/>
                         
-                        <h4>Database Management</h4>
-                        <TechCollection techMastered={TECHS.DATABASE_MANAGEMENT_MASTERED}/>
+                        <h4 className="animate glow delay-3">Database Management</h4>
+                        <TechCollection techMastered={TECHS.DATABASE_MANAGEMENT_MASTERED} className="animate glow delay-3"/>
 
-                        <h4>Others</h4>
-                        <TechCollection techMastered={TECHS.OTHER_TECHS}/>
+                        <h4 className="animate glow delay-3">Others</h4>
+                        <TechCollection techMastered={TECHS.OTHER_TECHS} className="animate glow delay-3"/>
 
                     </div>
 
